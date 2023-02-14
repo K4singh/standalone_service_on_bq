65937b39ce6e4151c91eb5f6c2393ec35ea3b342
Kinjal Singh <singhkinjal@google.com>
2023-02-14 15:23:31 +0530

    added http response but with error

--- examples/src/main/java/com/google/helloWorld.java
+++ examples/src/main/java/com/google/helloWorld.java
@@ -1,17 +1,40 @@
 package com.google;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.remoteFunctionObject;
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import com.google.gson.*;
+import java.io.BufferedWriter;
+import java.util.logging.Logger;
+public class helloWorld implements HttpFunction  {
+    private static final Gson gson = new Gson();
+    private static final Logger logger = Logger.getLogger(helloWorld.class.getName());
 
-public class helloWorld {
 
-    public static String generateString( String input) {
+    @Override
+    public void  service( HttpRequest request, HttpResponse response) throws Exception  {
 //        System.out.println("Generating string");
-
-        return input+"_test";
+        JsonElement requestParsed = gson.fromJson(request.getReader(), JsonElement.class);
+        JsonObject requestJson = requestParsed.getAsJsonObject();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String[][] calls = null;
+        if (requestJson != null) {
+            logger.info(">> Request Json: " + requestJson);
+            JsonNode jsonNode = objectMapper.readTree(String.valueOf(requestJson));
+            remoteFunctionObject remoteFunctionObject = objectMapper.treeToValue(jsonNode,
+                    remoteFunctionObject.class);
+            calls = remoteFunctionObject.getCalls();
+            logger.info(">>calls: " + calls);
+        }
+        return calls+"_test";
     }
 
     public static void main(String[] args){
         String sample = "this_is_a_string";
-        String encoded = generateString(sample);
-        System.out.println(encoded);
+//        String encoded = generateString(sample);
+        System.out.println(sample);
 
     }
 }
