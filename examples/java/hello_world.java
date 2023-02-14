cb40418a177ff1a61371b889e598066b20d16b9d
Kinjal Singh <singhkinjal@google.com>
2023-02-14 10:39:28 +0530

    first java example

---
+++ examples/java/hello_world.java
@@ -0,0 +1,15 @@
+public class hello_world {
+
+    public static String generateString( String input) {
+//        System.out.println("Generating string");
+
+        return input+"_test";
+    }
+
+    public static void main(String[] args){
+        String sample = "this_is_a_string";
+        String encoded = generateString(sample);
+        System.out.println(encoded);
+
+    }
+}
